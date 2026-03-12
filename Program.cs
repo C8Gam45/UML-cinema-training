465651e8d215a0f1c369e4357eff2240427f7ef9
Hazart Clovis <clohaz8@gmail.com>
2026-03-12 15:48:38 +0100

    exo1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Movie
+{
+    public string Title { get; set; }
+    public int Duration { get; set; } // in minutes
+
+    public void Display()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Duration: {Duration} minutes");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Movie movie = new Movie();
+        movie.Title = "Inception";
+        movie.Duration = 148;
+        movie.Display();
+    }
+}
